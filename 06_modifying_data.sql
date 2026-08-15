31797ccc103827480a7b656513cb625cc490d247
gautamrs224-r <gautamr.s155@gmail.com>
2026-08-15 21:28:50 +0530

    Updated Modifying_data

--- 06_modifying_data.sql
+++ 06_modifying_data.sql
@@ -1,2 +1,14 @@
 -- UPDATE - Modifying Existing Data
+SELECT * FROM users; -- Displaying the current data in the 'users' table before the update.
+SELECT * FROM users WHERE id = 5; -- Displaying the data for the user with id 5 before the update.
+-- Updating a Single Row
+UPDATE users SET name = 'Karam Updated', email = 'karam.updated@example.com' WHERE id = 5;
+
+-- Displaying the data for the user with id 6,7 and 8 before the update.
+SELECT * FROM users WHERE id = 6; 
+SELECT * FROM users WHERE id = 7;
+SELECT * FROM users WHERE id = 8;
+-- Updating Multiple Rows
+UPDATE users SET city = 'Updated City' WHERE id IN (6, 7, 8); -- Updating the city for users with id 6, 7, and 8.
+
 
